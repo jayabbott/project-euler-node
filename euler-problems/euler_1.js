4f6f0df6b1cd5edb2c30d19eeeabcf3b63b106b2
Jay Abbott <jayabbott94@gmail.com>
2020-01-08 09:04:37 +0000

    Completed problem 1

--- euler-problems/euler_1.js
+++ euler-problems/euler_1.js
@@ -1,1 +1,35 @@
+function listMultiplesOfUnder(multipleA, multipleB, below) {
+    var returnList = []
+    for (var i = 1; i < below; i++)
+    {
+        if (i % multipleA == 0)
+        {
+            returnList.push(i)
+        }
+        else if (i % multipleB == 0)
+        {
+            returnList.push(i)
+        }
+    }
+    return returnList
+}
+
+const arraySum = array => array.reduce((a,b) => a + b, 0)
+
+// Start
 console.log('Euler 1')
+
+// Example
+var exampleList = listMultiplesOfUnder(3, 5, 10)
+console.log('natural numbers below 10 that are multiples of 3 or 5')
+console.log(exampleList)
+console.log('sum:')
+console.log(arraySum(exampleList))
+
+
+// Challenge
+var challengeList = listMultiplesOfUnder(3, 5, 1000)
+console.log('natural numbers below 1000 that are multiples of 3 or 5')
+//console.log(challengeList) // We don't want to print this as it's like 400 items
+console.log('sum:')
+console.log(arraySum(challengeList))
